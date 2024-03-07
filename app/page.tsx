97742b19cd536e853312408ae004b2ecb872f53b
cby <2224533193@qq.com>
2024-03-07 10:02:03 +0800

    Bug Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import calendarIcon from "@/svgs/calendarIcon.svg";
-import { Button } from "./components/IconButton";
-// import { Button } from "./components/recipes/Button";
+import calendarIconLight from "@/svgs/calendarIconLight.svg";
+import { IconButton } from "./components/IconButton";
 export default function Home() {
   return (
     <>
-      <Button variant="grey" size="md">
+      <IconButton variant="grey" size="md" icon={calendarIcon}>
         <span style={{ marginLeft: "20px" }}>123</span>
-      </Button>
-      <Button variant="dark" size="md">
+      </IconButton>
+      <IconButton variant="dark" size="md" icon={calendarIconLight}>
         <span style={{ marginLeft: "20px" }}>456</span>
-      </Button>
-      {/* <Button variant={"dark"} size={"sm"}></Button>
-      <Button variant={"grey"} size={"sm"}></Button> */}
+      </IconButton>
     </>
   );
 }
